64f793f3cda23d5f738e3e80f96782e8dc0d5a70
Nelreina <nelreina@gmail.com>
2018-04-03 01:57:41 -0400

    trying send all export of auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import createStoreDOM from './redux/index';
 import auth from './redux/auth';
+import * as authActions from './redux/auth';
 import { get, post } from './api';
 // import BootstrapField from './components/BootstrapField';
 const api = { get, post };
-export { createStoreDOM, auth, api };
+export { createStoreDOM, auth, authActions, api };
